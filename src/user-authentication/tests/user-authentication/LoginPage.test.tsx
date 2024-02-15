577e31058ede01c91adf01ed3f8593ad04a37853
kevin collier <collier.kevin.m@gmail.com>
2024-02-15 09:36:23 +0900

    created and passed test that the login page calls the login service with credentials

--- src/user-authentication/tests/user-authentication/LoginPage.test.tsx
+++ src/user-authentication/tests/user-authentication/LoginPage.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import LoginPage from '../../LoginPage';
-// import * as loginService from '../../LoginService';
+import * as loginService from '../../LoginService';
 import { MemoryRouter } from 'react-router-dom';
 import { LoginFormProps } from '../../LoginFormProps';
 
@@ -28,5 +28,27 @@ describe('LoginPage with mocked LoginForm', () => {
     );
 
     expect(screen.getByRole('button', { name: /login/i })).toBeInTheDocument();
+  })
+
+  it('calls login service with credentials and redirects on success', async () => {
+    const mockLogin = loginService.login as jest.MockedFunction<typeof loginService.login>;
+    const fakeToken = 'fake_token';
+    mockLogin.mockResolvedValue({ token: fakeToken });
+
+    render(
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>
+    )
+
+    fireEvent.click(screen.getByRole('button', { name: /login/i }));
+
+    await waitFor(() => {
+      expect(mockLogin).toHaveBeenCalledWith('testUser', 'testPassword');
+      // Adjust for localStorage later on with something such as:
+      // expect(localStorage.setItem).toHaveBeenCalledWith('token', fakeToken);
+      // Mock out naviagation and test that as well
+      // Better yet -> separate this into two tests later on
+    })
   })
 })
