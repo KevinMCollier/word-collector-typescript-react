8ecd1865c468cced6d60dc5051e6aff7135da0ff
kevin collier <collier.kevin.m@gmail.com>
2024-02-23 20:06:13 +0900

    installed redux toolkit and created basic store.ts config

---
+++ src/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import authSlice from '../user-authentication/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    // Setup reducers here
+    // auth: authSlice
+  }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
