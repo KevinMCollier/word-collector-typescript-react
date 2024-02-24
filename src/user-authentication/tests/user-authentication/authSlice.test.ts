ccc85f5d2cb4a4e89024a3b0703bda8a1f7d70b1
kevin collier <collier.kevin.m@gmail.com>
2024-02-24 22:42:55 +0900

    created failing test for storing email and token in state upon login

--- src/user-authentication/tests/user-authentication/authSlice.test.ts
+++ src/user-authentication/tests/user-authentication/authSlice.test.ts
@@ -5,4 +5,12 @@ describe('authSlice', () => {
   it('should initialize state with no user and no token', () => {
     expect(store.getState().auth).toEqual({ user: null, token: null });
   })
+
+  it('stores user email and token in state upon login', () => {
+    store.dispatch(login({ email: 'user@example.com', token: 'fake_token' }));
+    expect(store.getState().auth).toEqual({
+      user: { email: 'user@example.com' },
+      token: 'fake_token',
+    });
+  });
 })
